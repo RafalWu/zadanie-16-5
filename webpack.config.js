431c9da03989fd45d79aed5a1444f1e66e7e3cba
Rafal Wrobel <wrobelrafal1992@gmail.com>
2018-04-19 13:46:50 +0200

    Modify

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,11 @@
 const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+plugins: [new HtmlWebpackPlugin({
+  template: 'src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})]
 
 module.exports = {
     entry: './src/app.js',
