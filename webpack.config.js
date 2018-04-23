cc939f2867fe3cefd38f4f8beb30025b96ab5686
Rafal Wrobel <wrobelrafal1992@gmail.com>
2018-04-23 17:20:18 +0200

    Modify config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,23 @@
 const path = require('path');
+var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
-plugins: [new HtmlWebpackPlugin({
-  template: 'src/index.html',
-  filename: 'index.html',
-  inject: 'body'
-})]
+
 
 module.exports = {
-    entry: './src/app.js',
+    entry: './src/index.js',
         output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'app.bundle.js'
     },
+	plugins: 
+	[new HtmlWebpackPlugin({
+	  template: 'src/index.html',
+	  filename: 'index.html',
+	  inject: 'body'
+	}),
+	new webpack.optimize.UglifyJsPlugin()],
     module: {
 		rules: [
 			{
