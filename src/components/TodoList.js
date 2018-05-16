b39ee9c5a1078b1576599fb6292f74eb5500c65e
Rafal Wrobel <wrobelrafal1992@gmail.com>
2018-05-16 10:17:40 +0200

    3

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,8 +1,8 @@
 import React from 'react';
-//import Todo from './Todo'
+import Todo from './Todo'
 
 const TodoList = props => {
-	//const todoListItems = props.data.map(item => <Todo key={item.id} id={item.id} todo={item.text} remove={props.remove}/>)
+	const todoListItems = props.data.map(item => <Todo key={item.id} id={item.id} todo={item.text} remove={props.remove}/>)
 	return (
 		<ul className={style.TodoList}>{todoListItems}</ul>
 	)
