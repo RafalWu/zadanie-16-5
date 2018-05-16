d6cdcc86c88510c93d53131c46e153032c8f979e
RafalWu <34887565+RafalWu@users.noreply.github.com>
2018-05-16 13:54:26 +0200

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Todo from './Todo'
 
 const TodoList = props => {
-	const todoListItems = props.data.map(item => <Todo key={item.id} id={item.id} todo={item.text} remove={props.remove}/>)
+
 	return (
 		<ul className={style.TodoList}>{todoListItems}</ul>
 	)
